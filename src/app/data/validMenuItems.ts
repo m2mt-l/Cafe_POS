de81c776eb198d27567d59288fc32038aa1992fe
m2mt-l <ta28ma2@gmail.com>
2022-05-31 23:37:45 +0000

    feat: add MENUITEMS

--- src/app/data/validMenuItems.ts
+++ src/app/data/validMenuItems.ts
@@ -1,13 +1,13 @@
 import { MenuItem } from '../model/menuItem';
 
 export const MENUITEMS: MenuItem[] = [
-    { name: 'Hot Coffee', imageUrl: '', price: 300 },
-    { name: 'Iced Coffee', imageUrl: '', price: 300 },
-    { name: 'Hot Latte', imageUrl: '', price: 300 },
-    { name: 'Iced Latte', imageUrl: '', price: 300 },
-    { name: 'Espresso', imageUrl: '', price: 300 },
-    { name: 'Cappuccino', imageUrl: '', price: 300 },
-    { name: 'Cheese Cake', imageUrl: '', price: 300 },
-    { name: 'Chiffon Cake', imageUrl: '', price: 300 },
-    { name: 'Chocolate cake', imageUrl: '', price: 300 },
+    { name: 'Hot Coffee', imageUrl: '../assets/hot-coffee.jpg', price: 4.0 },
+    { name: 'Iced Coffee', imageUrl: '../assets/iced-coffee.jpg', price: 4.5 },
+    { name: 'Hot Latte', imageUrl: '../assets/hot-latte.jpg', price: 5.0 },
+    { name: 'Iced Latte', imageUrl: '../assets/iced-latte.jpg', price: 5.5 },
+    { name: 'Espresso', imageUrl: '../assets/espresso.jpg', price: 5.0 },
+    { name: 'Cappuccino', imageUrl: '../assets/cappuccino.jpg', price: 6.0 },
+    { name: 'Cheese Cake', imageUrl: '../assets/cheese-cake.jpg', price: 6.0 },
+    { name: 'Chiffon Cake', imageUrl: '../assets/chiffon-cake.jpg', price: 5.5 },
+    { name: 'Chocolate Cake', imageUrl: '../assets/chocolate-cake.jpg', price: 6.0 },
 ] 
