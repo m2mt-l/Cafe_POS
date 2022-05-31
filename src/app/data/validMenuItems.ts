bd119e5632810b18803441a7e4d258d741f3dabf
m2mt-l <ta28ma2@gmail.com>
2022-05-31 15:00:31 +0000

    feat: create MENUITEMS

---
+++ src/app/data/validMenuItems.ts
@@ -0,0 +1,13 @@
+import { MenuItem } from '../model/menuItem';
+
+export const MENUITEMS: MenuItem[] = [
+    { name: 'Hot Coffee', imageUrl: '', price: 300 },
+    { name: 'Iced Coffee', imageUrl: '', price: 300 },
+    { name: 'Hot Latte', imageUrl: '', price: 300 },
+    { name: 'Iced Latte', imageUrl: '', price: 300 },
+    { name: 'Espresso', imageUrl: '', price: 300 },
+    { name: 'Cappuccino', imageUrl: '', price: 300 },
+    { name: 'Cheese Cake', imageUrl: '', price: 300 },
+    { name: 'Chiffon Cake', imageUrl: '', price: 300 },
+    { name: 'Chocolate cake', imageUrl: '', price: 300 },
+] 
