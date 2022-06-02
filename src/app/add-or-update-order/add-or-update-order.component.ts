8f1d96ab0de226d00b456fe84805bc23064e4a07
m2mt-l <ta28ma2@gmail.com>
2022-06-02 23:26:07 +0000

    feat: add sum total

--- src/app/add-or-update-order/add-or-update-order.component.ts
+++ src/app/add-or-update-order/add-or-update-order.component.ts
@@ -8,9 +8,19 @@ import { MenuItem } from '../model/menuItem';
 })
 export class AddOrUpdateOrderComponent implements OnInit {
     customerName: string = '';
+    total: number[] = [];
     
     constructor(private menuItemsService: MenuItemService) {}
 
     menuItems: MenuItem[] = this.menuItemsService.getAll();
     ngOnInit(): void {}
+
+    sumOrderTotal(index: number, inputNumber: string, menuItem: MenuItem): void {
+        this.total[index] = Number(inputNumber) * menuItem.price;
+    }
+
+    getTotal(): number {
+        if(this.total.length === 0) return 0;
+        else return this.total.reduce((total, x) => total + x);
+    }
 }
