404f1a6875982486db7fd260cb9be9541a59e4ae
m2mt-l <ta28ma2@gmail.com>
2022-06-05 14:04:38 +0000

    fix: lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
         MatInputModule,
         MatSortModule,
         MatTabsModule,
-        MatExpansionModule
+        MatExpansionModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
