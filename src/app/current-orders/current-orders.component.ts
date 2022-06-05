0b836f524a3850ff7ddcb349eaece879a4cff612
m2mt-l <ta28ma2@gmail.com>
2022-06-05 00:24:41 +0000

    feat: completed order

--- src/app/current-orders/current-orders.component.ts
+++ src/app/current-orders/current-orders.component.ts
@@ -13,7 +13,7 @@ export class CurrentOrdersComponent implements OnInit {
     ngOnInit(): void {}
 
     setComplete(order: Order): void{
+        order.updateTime();
         order.setCompleted();
-        console.log(order);
     }
 }
