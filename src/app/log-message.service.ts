c4702d0e23ffee6ddd297994b308efd80cbdf2a2
m2mt-l <ta28ma2@gmail.com>
2022-06-09 23:51:40 +0000

    feat: add and clear messages

--- src/app/log-message.service.ts
+++ src/app/log-message.service.ts
@@ -4,7 +4,17 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class LogMessageService {
+    messages: string[] = [];
+    //FIX delete
     testMessage: string = 'This is from log message service.';
-    
+
     constructor() { }
+
+    addMessage(message: string) {
+        this.messages.push(message);
+    }
+
+    clearMessage() {
+        this.messages = [];
+    }
 }
